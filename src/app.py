b1a437dd4d4eb82197e20262edb631831cce6795
Filip Synek <xjust@mendelu.cz>
2022-12-22 12:59:45 +0100

    Added shell context processor
    - When running `flask shell` app instance should have access to right `db` instance and also to `User` and `Role` models
    - Reorganized imports in the file

--- src/app.py
+++ src/app.py
@@ -1,3 +1,10 @@
-from src.app import create_app
+from app import create_app, db
+from app.models.role import Role
+from app.models.user import User
 
 app = create_app('default')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
