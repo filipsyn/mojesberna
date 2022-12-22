b5c19f0ada68ef9640f7cfa577fe4aedd5c9428f
Filip Synek <xjust@mendelu.cz>
2022-12-22 19:12:31 +0100

    Added `PriceList` model

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,5 +1,6 @@
 from .address import Address
 from .material import Material
+from .price_list import PriceList
 from .purchase import Purchase
 from .role import Role
 from .user import User
---
+++ src/app/models/price_list.py
@@ -0,0 +1,5 @@
+from .. import db
+
+
+class PriceList(db.Model):
+    pass
