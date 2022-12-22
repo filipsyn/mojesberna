f148619f019be9d10ebc7442468581edf0db0aa1
Filip Synek <xjust@mendelu.cz>
2022-12-22 19:09:34 +0100

    Added string magic function to `Purchase` model

--- src/app/models/purchase.py
+++ src/app/models/purchase.py
@@ -10,3 +10,6 @@ class Purchase(db.Model):
     weight = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"Purchase id: {self.purchase_id} - {self.weight} kg on {self.date}"
