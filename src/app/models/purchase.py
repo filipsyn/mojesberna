879cc2605980ddc6097f1dd5f6730ce95d81c0be
Filip Synek <xjust@mendelu.cz>
2022-12-22 19:02:56 +0100

    Specified tablename and applied formatting

--- src/app/models/purchase.py
+++ src/app/models/purchase.py
@@ -1,8 +1,11 @@
-from .. import db
 from datetime import datetime
+
+from .. import db
 
 
 class Purchase(db.Model):
+    __tablename__ = 'purchases'
+
     purchase_id = db.Column(db.Integer, primary_key=True)
     weight = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
