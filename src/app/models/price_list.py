06f9c3c08041636287d712ef697a9f9fa2688d02
Filip Synek <xjust@mendelu.cz>
2022-12-22 19:16:15 +0100

    Added implementation of `PriceList` model

--- src/app/models/price_list.py
+++ src/app/models/price_list.py
@@ -1,5 +1,14 @@
+from datetime import datetime
+
 from .. import db
 
 
 class PriceList(db.Model):
-    pass
+    __tablename__ = 'price_list'
+
+    price_id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Price id: {self.price_id} - {self.price} Kč on {self.date}"
