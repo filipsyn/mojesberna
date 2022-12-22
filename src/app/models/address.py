fae86f6942c3f107c9ccdf43fbd7e332b267047b
Filip Synek <xjust@mendelu.cz>
2022-12-22 18:50:14 +0100

    Added implementation of `Address` model

--- src/app/models/address.py
+++ src/app/models/address.py
@@ -2,4 +2,12 @@ from .. import db
 
 
 class Address(db.Model):
-    pass
+    __tablename__ = 'address'
+    address_id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(64), nullable=False)
+    street = db.Column(db.String(64))
+    house_number = db.Column(db.String(16), nullable=False)
+    zip_code = db.Column(db.String(16), nullable=False)
+
+    def __repr__(self):
+        return f"Address: id {self.address_id} - {self.house_number} {self.street}, {self.city}, {self.zip_code}"
