76034386d9f749fef4524c2ee0e3d80ed3ad7546
Filip Synek <xjust@mendelu.cz>
2022-12-22 19:02:14 +0100

    Added implementation of `Purchase` model

--- src/app/models/purchase.py
+++ src/app/models/purchase.py
@@ -1,5 +1,9 @@
 from .. import db
+from datetime import datetime
 
 
 class Purchase(db.Model):
-    pass
+    purchase_id = db.Column(db.Integer, primary_key=True)
+    weight = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text)
