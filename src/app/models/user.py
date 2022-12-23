cc53e8e1a28427dcf18944bfab8c0d98a9ae234d
Filip Synek <xjust@mendelu.cz>
2022-12-23 19:13:57 +0100

    Added login to `User` model
    - also applied migration to database

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -7,6 +7,7 @@ class User(db.Model):
     first_name = db.Column(db.String(64), nullable=False)
     last_name = db.Column(db.String(64))
     telephone_number = db.Column(db.String(17))
+    login = db.Column(db.String(128), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     permanent_residence_id = db.Column(db.Integer, db.ForeignKey('addresses.address_id'))
     temporary_residence_id = db.Column(db.Integer, db.ForeignKey('addresses.address_id'))
@@ -14,4 +15,4 @@ class User(db.Model):
     purchases = db.relationship('Purchase', backref='user')
 
     def __repr__(self):
-        return f"User: id:{self.user_id} - name:{self.first_name} {self.last_name}"
+        return f"User: id:{self.user_id} - name:{self.first_name} {self.last_name} login: {self.login}"
