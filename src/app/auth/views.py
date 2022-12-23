61c59ee4d38ccbc1457c1c371e879a9928740743
Filip Synek <xjust@mendelu.cz>
2022-12-23 18:07:10 +0100

    Added template for registration page

--- src/app/auth/views.py
+++ src/app/auth/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/register')
 def view_register_page():
-    return "register page"
+    return render_template('auth/register.jinja2', title='Registrace')
