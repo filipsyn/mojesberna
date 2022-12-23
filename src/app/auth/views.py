d6fe6d51ab997094d4538176c9e7ee4ca49fbb68
Filip Synek <xjust@mendelu.cz>
2022-12-23 19:47:00 +0100

    Added instanciation of registration form into register site

--- src/app/auth/views.py
+++ src/app/auth/views.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
 
+from .forms import RegisterForm
+
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def view_register_page():
-    return render_template('auth/register.jinja2', title='Registrace')
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/register.jinja2', title='Registrace', form=form)
