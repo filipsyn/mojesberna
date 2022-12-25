6d482be377ca5c0958979aaf782ad96fb10814b9
Filip Synek <xjust@mendelu.cz>
2022-12-25 20:41:45 +0100

    Implemented `Status` model

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -3,4 +3,5 @@ from .material import Material
 from .price_list import PriceList
 from .purchase import Purchase
 from .role import Role
+from .status import Status
 from .user import User
--- src/app/models/status.py
+++ src/app/models/status.py
@@ -2,4 +2,32 @@ from .. import db
 
 
 class Status(db.Model):
-    pass
+    __tablename__ = 'statuses'
+    status_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    default = db.Column(db.Boolean, default=False)
+    users = db.relationship('User', backref='status')
+
+    def __init__(self, name: str, default: bool = False):
+        self.name = name
+        self.default = default
+
+    def __repr__(self):
+        return f"<Status ID {self.status_id}; {self.name}; (default {self.default})"
+
+    @staticmethod
+    def insert_statuses():
+        statuses = [
+            'Waiting',
+            'Active',
+            'Banned'
+        ]
+        default_status = statuses[0]
+
+        for s in statuses:
+            status = Status.query.filter_by(name=s).first()
+            if status is None:
+                status = Status(s)
+            status.default = (s == default_status)
+            db.session.add(status)
+        db.session.commit()
