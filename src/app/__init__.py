1ec0dcb88099cad83156093a710de1b133536665
Filip Synek <xjust@mendelu.cz>
2022-12-25 13:35:57 +0100

    Implemented Login Manager

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,6 +8,7 @@ from .config import config
 # Creating objects for external applications
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app(config_name='default') -> Flask:
@@ -25,6 +27,7 @@ def create_app(config_name='default') -> Flask:
     # Initialization of external packages
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     # Importing blueprints
     from .main.views import main as main_blueprint
--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,4 +1,13 @@
-from .. import db
+from enum import Enum
+
+from flask_login import UserMixin
+
+from .. import db, login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model):
