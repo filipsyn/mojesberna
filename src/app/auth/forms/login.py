a3825cd38d40175ab68b9cdf7ca48bc00a2ad2c4
Filip Synek <xjust@mendelu.cz>
2022-12-25 16:31:05 +0100

    Implemented `LoginForm`

--- src/app/auth/forms/login.py
+++ src/app/auth/forms/login.py
@@ -1,5 +1,9 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    pass
+    login = StringField('', validators=[DataRequired()])
+    password = PasswordField('', validators=[DataRequired()])
+    remember_login = BooleanField('')
