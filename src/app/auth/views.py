6baba0d0d329ab6323bdad30d3ac493e50b2631d
Filip Synek <xjust@mendelu.cz>
2022-12-25 12:46:30 +0100

    Implemented basics of user registration
    - Specified missing `POST` method to the registration form
    - Implemented creation of new user on `/register` endpoint

--- src/app/auth/views.py
+++ src/app/auth/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 
 from .forms import RegisterForm
+from .. import db
+from ..models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -10,5 +12,14 @@ def view_register_page():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        pass
+        new_user = User(
+            form.first_name.data,
+            form.last_name.data,
+            form.telephone_number.data,
+            form.login.data,
+            form.password.data
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
     return render_template('auth/register.jinja2', title='Registrace', form=form)
