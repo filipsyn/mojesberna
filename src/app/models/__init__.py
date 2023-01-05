35cbb50941e11fe9da1f78d702caef8eba68ce5c
Filip Synek <xjust@mendelu.cz>
2023-01-05 19:46:38 +0100

    Export of `Permission` enum

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -2,7 +2,6 @@ from .address import Address
 from .material import Material
 from .price_list import PriceList
 from .purchase import Purchase
-from .role import Role
-from .status import Status
-from .status import UserStatus
+from .role import Role, Permission
+from .status import Status, UserStatus
 from .user import User
