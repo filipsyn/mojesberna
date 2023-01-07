71984670b081be7ca7f031756080773a770c6fef
Filip Synek <xjust@mendelu.cz>
2023-01-07 18:35:59 +0100

    Added "statistics" section to landing page
    - Added markup to the `.jinja2` file
    - Added styling of this section
    - Added placeholder data declaration into endpoint function

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -5,7 +5,13 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def view_home_page():
-    return render_template('main/homepage.jinja2', title='Domovská stránka')
+    data = {
+        'Noviny': 1200,
+        'Železo': 980,
+        'Měď': 375,
+        'Hliník': 525
+    }
+    return render_template('main/homepage.jinja2', title='Domovská stránka', data=data)
 
 
 @main.route('/greet')
