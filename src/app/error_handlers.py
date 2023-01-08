b6a0e19c12cf6e5bc177d178117fa26024e3b124
Filip Synek <xjust@mendelu.cz>
2023-01-08 13:28:04 +0100

    Added error handler endpoints

---
+++ src/app/error_handlers.py
@@ -0,0 +1,14 @@
+def error_page_not_found(e):
+    raise NotImplementedError
+
+
+def error_unauthorized(e):
+    raise NotImplementedError
+
+
+def error_forbidden(e):
+    raise NotImplementedError
+
+
+def error_internal_server_error(e):
+    raise NotImplementedError
