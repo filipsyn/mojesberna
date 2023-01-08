9b1ca3fe2b3cfc8bcbdb223b0554f2900f5ad295
Filip Synek <xjust@mendelu.cz>
2023-01-08 18:59:18 +0100

    Querying for registration requests + placeholder data declaration

--- src/app/user/views.py
+++ src/app/user/views.py
@@ -29,9 +29,25 @@ def view_change_password_page():
 @permission_required(Permission.ACCESS)
 def dashboard_page():
     user_attributes = get_user_attributes(current_user)
-    price_list = Material.query.join(PriceList, Material.material_id == PriceList.material_id).add_columns(
-        Material.material_id, PriceList.price_id, Material.name, PriceList.price).all()
-    registration_request = User.query.filter_by(status_id=1).all()
+    price_list = Material.query. \
+        join(PriceList, Material.material_id == PriceList.material_id) \
+        .add_columns(Material.material_id, PriceList.price_id, Material.name, PriceList.price) \
+        .all()
+
+    registration_request = User.query \
+        .filter_by(status_id=1) \
+        .order_by(User.user_id) \
+        .limit(3) \
+        .all()
+
+    price_list = {
+        'Noviny': 2.70,
+        'Železo': 4.50,
+        'Měď': 85,
+        'Mosaz': 45,
+        'Olovo': 20
+    }
+
     return render_template("user/dashboard.jinja2", title=f"Přehled uživatele {current_user.login}",
                            user_attributes=user_attributes, price_list=price_list,
                            registration_request=registration_request)
